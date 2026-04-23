ede0c8b4cad866644d7854e7ecc77fb6affe6caa
Usman Ganaev <usman.ganaev@giftery.pro>
2026-04-23 15:52:06 +0400

    Fix: Allowed POST and DELETE methods for group management API

--- backend/server.py
+++ backend/server.py
@@ -50,7 +50,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
